9beacd90e75c59853096633965f78bbf1b2c8923
Vimal Raveendran <vimalraveendra@gmail.com>
2021-06-11 06:11:06 +0200

    initial commit

---
+++ Sample/Data/Models/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Sample.Data.Models
+{
+    public class TaskModel
+    {
+       public string TaskName { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
